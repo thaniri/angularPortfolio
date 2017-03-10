3ef133ca05fa93a58129f4e7eaceaf683dec945a
Thaniri <thaniri@gmail.com>
2017-03-09 23:15:04 -0800

    added css, changed html location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,35 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app',
   template: `
-    <nav>
-      <ul>
-        <li><a [routerLink]="['./home-page']">Home Page</a></li>
-        <li><a [routerLink]="['./portf-page']">Portfolio</a></li>
-        <li><a [routerLink]="['./transcript-page']">Transcript</a></li>
-      </ul>
-    </nav>
-    <main>
-      <div>
-        <router-outlet></router-outlet>
-      </div>
-    </main>
+    <div id="leftCol">
+      <nav>
+        <div id="topNav">
+          <a [routerLink]="['./home-page']">Marek Slabicki</a>
+        </div>
+        <div id="centerNav">
+          <ul>
+            <li><a [routerLink]="['./portf-page']">Portfolio</a></li>
+            <li><a [routerLink]="['./transcript-page']">Transcript</a></li>
+          </ul>
+        </div>
+        <div id="botNav">
+          <span>Marek Slabicki</span>
+        </div>
+      </nav>
+    </div>
+    <div id="rightCol">
+      <header>
+          <p>header</p>
+      </header>
+      <main>
+        <div>
+          <router-outlet></router-outlet>
+        </div>
+      </main>
+      <footer>
+        <p>footer</p>
+      </footer>
+    </div>
   `
 })
 
